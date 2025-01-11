cbc48f5aa2da5a331586d6bf3965e3c0a98aed1f
valya_plotnikova <valya_ezhova@mail.ru>
2025-01-11 14:53:23 +0300

    Revert "create async config"
    This reverts commit 95c08d48357781f134ab39508343c75694328a08.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from models.database import create_db, drop_db
-import asyncio
 from parser import parsing_trading_on_file, get_data, save_data_to_db
 
 
-async def main():
-    await drop_db()
-    await create_db()
+def main():
+    drop_db()
+    create_db()
+    trade_date = parsing_trading_on_file()
+    spimex_trading_results = get_data(trade_date)
+    save_data_to_db(spimex_trading_results)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
--- models/database.py
+++ models/database.py
@@ -1,24 +1,21 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 Base = declarative_base()
 
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
 
-async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-
-async def create_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+def create_db():
+    Base.metadata.create_all(bind=engine)
     print('Таблица успешно создана')
 
 
-async def drop_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-    print('Таблица успешно удалена')
+def drop_db():
+    Base.metadata.drop_all(bind=engine)
